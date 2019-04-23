ef387d159dde2261c1fc5507d6ea879cfbac5d55
Pragith Prakash <pragith.prakash@cardinalpath.com>
2019-04-23 14:48:13 -0300

    Added last_record.csv for improving the efficiency.

--- time_tracker.py
+++ time_tracker.py
@@ -15,13 +15,13 @@ work_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Get the timesheet file path
 timesheet_file = work_dir + r'\timesheet.csv'
+last_record_file = work_dir + r'\last_record.csv' # This way it's faster to read the last task and write to the timesheet file without reading it completely
 
 # Read the projects list
 projects = read_csv(work_dir + r'\projects.csv')
 
 # Get the last record in the timesheet
-timesheet = read_csv(timesheet_file)
-last_record = timesheet.tail(1)
+last_record = read_csv(last_record_file)
 last_task = last_record.task.tolist()[0]
 last_project = last_record.project.tolist()[0]
 
@@ -47,8 +47,11 @@ def record_task(current_task, project_id):
 		}, index=[0])
 	# Push to csv
 	ts.to_csv(timesheet_file, header=False, mode='a', index=False)
+	ts.to_csv(last_record_file, index=False)
 
-def generate_report(timesheet):
+def generate_report():
+	timesheet = read_csv(timesheet_file, error_bad_lines=False)
+	
 	# Calculate the start and end dates of reports dynamically
 	end = pendulum.now()	
 	start = to_datetime(args['start_date']) if args['start_date'] else to_datetime(end.start_of('week').strftime('%Y-%m-%d'))
@@ -76,7 +79,7 @@ choice = 'report' if (args['report'] or args['start_date']) else 'task'
 while choice != '':
 	if choice == 'report':
 		# Generate this week in review report
-		generate_report(timesheet)		
+		generate_report()		
 	else:
 		# Enter current task
 		print('Time now:',time.strftime("%H:%M",time.localtime(now)))
